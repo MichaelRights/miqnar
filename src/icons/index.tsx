209c57b65d1fd2e2f50ba4ae762b3ce7ac2f9bff
vladimir-alena <111335294+Vlad-L8@users.noreply.github.com>
2024-08-31 11:46:37 +0400

    to deploy

---
+++ src/icons/index.tsx
@@ -0,0 +1,5 @@
+export { IconSm } from "./icon-sm";
+export { IconLg } from "./icon-lg";
+export { IconBaselinePhone } from "./icon-baseline-phone";
+export { IconLocationFilled } from "./icon-location-filled";
+export { IconMailSolid } from "./icon-mail-solid";
